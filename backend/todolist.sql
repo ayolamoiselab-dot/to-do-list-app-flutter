fd6834634cc3932531fd51df41b0d0dcec30f672
ayolamoiselab-dot <moiseayola4@gmail.com>
2025-04-12 11:55:56 +0000

    ajout fichier sql bdd

---
+++ backend/todolist.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : sam. 12 avr. 2025 à 13:53
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `todolist`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `todoitems`
+--
+
+CREATE TABLE `todoitems` (
+  `id` int(11) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `tasks` text NOT NULL,
+  `status` enum('a faire','en cours','termine') NOT NULL DEFAULT 'a faire',
+  `user_id` int(11) DEFAULT NULL,
+  `group_name` varchar(50) DEFAULT 'default',
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'moiseayola4@gmail.com', 'atalasse', '2025-04-10 23:03:45', '2025-04-10 23:03:45'),
+(2, 'moiseayola6@gmail.com', 'atalasse', '2025-04-12 11:13:00', '2025-04-12 11:13:00');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `todoitems`
+--
+ALTER TABLE `todoitems`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `todoitems`
+--
+ALTER TABLE `todoitems`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `todoitems`
+--
+ALTER TABLE `todoitems`
+  ADD CONSTRAINT `todoitems_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
